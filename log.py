aa3716820b1a422d4cbf5eac9f6b58a170d7ed2b
gowriprathap <gowrinp@gmail.com>
2019-05-28 13:29:10 -0400

    Styling according to PEP8 style guide

--- log.py
+++ log.py
@@ -3,50 +3,60 @@
 import psycopg2
 
 
-query1 = "SELECT title, count(*) AS views FROM articles JOIN log ON articles.slug = substring(log.path,10) GROUP BY title ORDER BY views DESC LIMIT 3;" #First query
-#Which articles have been accessed the most?
-#Presenting this information as a sorted list with the most popular article at the top
-query_result1 = dict() #Creating a dict object
+query1 = """SELECT title, count(*) AS views FROM articles JOIN log
+         ON articles.slug = substring(log.path,10)
+         GROUP BY title ORDER BY views DESC LIMIT 3;"""   # First query
+# Which articles have been accessed the most?
+# Sorted list with the most popular article at the top
+query_result1 = dict()  # Creating a dict object
 query_result1['title'] = '\nArticles which have been accessed the most\n'
 
 
-query2 = "SELECT authors.name, count(*) AS views FROM authors JOIN articles ON articles.author = authors.id JOIN log ON articles.slug = substring(log.path,10) GROUP BY authors.name ORDER BY views DESC;" #Second query
-#Who are the most popular article authors of all time?
-#when you sum up all of the articles each author has written, which authors get the most page views?
-#Presenting this as a sorted list with the most popular author at the top.
+query2 = """SELECT authors.name, count(*) AS views FROM authors
+         JOIN articles ON articles.author = authors.id JOIN log
+         ON articles.slug = substring(log.path,10) GROUP BY authors.name
+         ORDER BY views DESC;"""  # Second query
+# Who are the most popular article authors of all time?
+# Which authors get the most page views?
+# Presenting this as a sorted list with the most popular author at the top.
 query_result2 = dict()
 query_result2['title'] = '\nMost popular authors of all time\n'
 
 
-query3 = "SELECT to_char(day2, 'Mon DD, YYYY'), round((numstats*100.0)/visitors, 3) AS result FROM errorsum ORDER BY result DESC LIMIT 1" #Third query
-#On which days did more than 1% of requests lead to errors?
+query3 = """SELECT to_char(day2, 'Mon DD, YYYY'),
+         round((numstats*100.0)/visitors, 3)
+         AS result FROM errorsum ORDER BY result DESC LIMIT 1"""  # Third query
+# On which days did more than 1% of requests lead to errors?
 query_result3 = dict()
-query_result3['title'] = '\nDays on which more than 1% of requests led to errors\n'
+query_result3['title'] = """\nDays with more than 1% of errors\n"""
 
 
-def get_query(query): #Function to get query
-    db = psycopg2.connect("dbname=news") #Connecting to the database
+def get_query(query):  # Function to get query
+    db = psycopg2.connect("dbname=news")  # Connecting to the database
     c = db.cursor()
     c.execute(query)
     results = c.fetchall()
-    db.close() #Closing the database
-    return results #Returning the results
+    db.close()  # Closing the database
+    return results  # Returning the results
 
 
-def print_query(query_results): #Printing first and second queries
-    print (query_results['title'])
+def print_query(query_results):  # Printing first and second queries
+    print(query_results['title'])
     for result in query_results['results']:
-        print ('\t' + str(result[0]) + ' - ' + str(result[1]) + ' views') #Printing the first two parts of the result
+        print('\t' + str(result[0]) + ' - ' + str(result[1]) + ' views')
+        # Printing the first two parts of the result
 
 
-def print_3rd_query(query_results): #Printing third query
-    print (query_results['title'])
+def print_3rd_query(query_results):  # Printing third query
+    print(query_results['title'])
     for result in query_results['results']:
-        print ('\t' + str(result[0]) + ' - ' + str(result[1]) + '% errors') #Printing as % error
+        print('\t' + str(result[0]) + ' - ' + str(result[1]) + '% errors')
+        # Printing as % error
 
 
-query_result1['results'] = get_query(query1) #Getting the query and storing in the dict object
-print_query(query_result1) #Printing the dict object
+query_result1['results'] = get_query(query1)
+# Getting the query and storing in the dict object
+print_query(query_result1)  # Printing the dict object
 query_result2['results'] = get_query(query2)
 print_query(query_result2)
 query_result3['results'] = get_query(query3)
